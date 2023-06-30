c0eb138f9b66dd96f8f776515986291e597815a1
Khadijatahmad <khadahmad000@gmail.com>
2023-06-30 07:55:17 -0700

    config magt

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a new file using Puppet
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
